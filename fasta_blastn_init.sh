b40de27576270514e032b56c96334768808a2436
Daniel Nimptsch <danielnimptsch@hotmail.de>
2022-12-06 17:46:13 +0100

    removed unneeded files

--- fasta_blastn_init.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# .fasta_blastn_init.sh
-# Custom env variables from soye
-export CONDASH="/home/soye/miniconda3/etc/profile.d/conda.sh"
-export ASVBLASTDB="/home/soye/Archive/pipeline_rechner/localBLAST/blastdb"
-THREADS=16
